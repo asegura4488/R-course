993d968fc2bf97fc906ae1b2217e13aeb738a2e2
asegura4488 <asegura4488@gmail.com>
2020-06-08 10:19:36 -0500

    Matrices

---
+++ Matrix_1.R
@@ -0,0 +1,35 @@
+
+print('--- Running ---')
+
+city1 <- c(20,20,16,17,17,22,16,2 ,6 ,18)
+city2 <- c(11,13,11,8 ,12,11,12,8, 5 ,14)
+city3 <- c(18,15,15,15,16,17,15,13,11,10)
+
+# Creating matrix from the vectors
+
+total <- matrix( c(city1,city2,city3),
+				 nrow = 10, ncol = 3 )
+
+# Rename the columns
+
+colnames(total) <- c('city1','city2','city3')
+# No commonly used rownames(total) <- c( ... ) 
+
+#total <- total * 5
+
+print(total)
+
+#Selecting elements
+print('Element 3,2')
+
+x <- total[3,2] # or
+
+x <- total[3,'city2']
+
+print(x)
+
+print(total[ c(3,2) ,  c(1,1) ])
+
+# selecting a whole field
+City = total[,'city1']
+print(City)
