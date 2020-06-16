2ff17549a715328d3df1d315810a6015bd9d21ae
asegura4488 <asegura4488@gmail.com>
2020-06-15 20:08:27 -0500

    tree

--- arbol.R
+++ arbol.R
@@ -7,22 +7,20 @@ library('RColorBrewer')
 df <- titanic_train
 #print(df)
 
-
 arbol <- rpart(
 		formula = Survived ~ Sex + Age,
 		data = titanic_train,
 		method = 'class' )
 
 pdf('TreepLot.pdf')
+rpart.plot(arbol, extra = 106)
+dev.off()
 
 predict <- predict(arbol, type = 'class')
 titanic_predict <- cbind(titanic_train, predict)
-
 print(titanic_predict)
-fancyRpartPlot(arbol)
-dev.off()
 
 test <- predict(object = arbol,
-				newdata = data.frame(Age = 4, Sex = 'male'),
+				newdata = data.frame(Age = 10, Sex = 'female'),
 				type = 'class')
 print(test)				
